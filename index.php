4d43608f5255bb0bd900ba135457aefe74aefc4e
isman7 <isman7@gmail.com>
2015-08-08 17:08:05 +0200

    Personal area with social data from using the Simple HTML DOM

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php 
+$path_dom = TEMPLATEPATH.'/src/php/simple_html_dom.php';
+
+include($path_dom); 
+?>
 
 <!DOCTYPE html>
 <html lang="es">
@@ -50,54 +55,80 @@
   <body onload="prettyPrint()">
 	<div class="row">
 		<div class="col-lg-4">
-			<div class="text-center">
-				<div class="row">
-					<div class="col-lg-3 col-sm-1"></div>
-					<div class="col-lg-7 col-sm-9 thumbnail voffset9" id="vcard">
-						<div class="row">
-							<div class="col-lg-6 col-md-7" style="margin-top: -40px;">
-							<a class="" href="" title="Isman Siete">
-								<img class="img-thumbnail img-responsive" src="https://avatars3.githubusercontent.com/u/9478220?v=3&s=460" width="" alt="">
-							</a>
-							</div>
-							<div class="col-lg-1"></div>
-							<div class="col-lg-5 text-left"  style="margin-top: -10px;">
-							<h4><strong>Ismael Benito</strong></h4>
-							<p>Intento de físico e ingeniero electrónico de telecomunicaciones. </p>
-							</div>
+			
+			<div class="row">
+				<div class="col-lg-3 col-sm-1"></div>
+				<div class="col-lg-7 col-sm-9 thumbnail voffset7" id="vcard">
+					<div class="row">
+						<div class="col-lg-6 col-md-7" style="margin-top: -40px;">
+						<a class="" href="" title="Isman Siete">
+							<img class="img-thumbnail img-responsive" src="https://avatars3.githubusercontent.com/u/9478220?v=3&s=460" width="" alt="">
+						</a>
 						</div>
-						<div class="row">
-						<div class="col-md-1"></div>
-						<div class="col-md-10">
-						<ul class="media-list">
-							<li class="media">
-							<a href="https://twitter.com/ismansiete" target="blank">
-								<div class="media-left">
-									<i class="fa fa-twitter"></i>
-								</div>
-								<div class="media-body">
-									<h4 class="media-heading">@ismansiete</h4>
-								</div>
-							</a>
-							</li>
-							<li class="media">
-							<a href="https://github.com/isman7" target="blank">
-								<div class="media-left">
-										<span class="fa fa-github"></span>
-								</div>
-								<div class="media-body">
-									<h4 class="media-heading">isman7</h4>
-								</div>
-							</a>
-							</li>
-						</ul>
-						</div>
-						<div class="col-md-1"></div>
+						<div class="col-lg-1"></div>
+						<div class="col-lg-5 text-left"  style="margin-top: -10px;">
+						<h4><strong>Ismael Benito</strong></h4>
+						<p>Intento de físico e ingeniero electrónico de telecomunicaciones. </p>
 						</div>
 					</div>
-					<div class="col-md-2"></div>
+					<div class="row">
+					<div class="col-md-1"></div>
+					<div class="col-md-10">
+					
+						
+							
+						<!--<i class="fa fa-twitter"></i>-->
+					
+					
+						<a href="https://twitter.com/ismansiete" target="blank">
+						<h4 class="media-heading">@ismansiete</h4>
+						</a>
+						<?php
+						$html_twitter = file_get_html('https://twitter.com/ismansiete'); 
+						$i = 0;
+						echo '<div class="row text-center">';
+						foreach($html_twitter->find('span.ProfileNav-value') as $e){
+						echo '<div class="col-lg-4">';
+						echo '<h4>'. $e->innertext . '</h4>';
+						$i++;
+							switch ($i) {
+							case 1:
+								echo '<span class="text-muted">Tweets</span></div>';
+								break 1;
+							case 2:
+								echo '<span class="text-muted">Siguiendo</span></div>';
+								break 1;
+							case 3:
+								echo '<span class="text-muted">Siguidores</span></div>';
+								break 2;
+							default:
+								break;
+							}
+						}
+						echo '</div>'; /* row */
+						?>
+							<!--<span class="fa fa-github"></span>-->
+						<a href="https://github.com/isman7" target="blank">
+						<h4 class="media-heading">isman7</h4>
+						</a>
+						<?php
+						$html_github = file_get_html('https://github.com/isman7');
+						$i = 0;
+						echo '<div class="row text-center">';
+						foreach($html_github->find('strong.vcard-stat-count') as $e)
+						$e->outertext = '<h4>'.$e->innertext.'</h4>';
+						foreach($html_github->find('a.vcard-stat') as $e){
+						echo '<div class="col-lg-4">' . $e->innertext . '</div>';
+						}
+						echo '</div>'; /* row */
+						?>
+					</div>
+					<div class="col-md-1"></div>
+					</div>
 				</div>
+				<div class="col-md-2"></div>
 			</div>
+			
 		</div>
   
   
