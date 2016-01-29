4e0f64bcef61cd29b3ba40aa7d759b610cf0f783
isman7 <isman7@gmail.com>
2016-01-29 16:33:30 +0100

    Better personal space

--- index.php
+++ index.php
@@ -41,7 +41,8 @@ include($path_dom);
     <link href="<?php echo get_template_directory_uri(); ?>/src/css/bootstrap.css" rel="stylesheet">
     <link href="<?php echo get_template_directory_uri(); ?>/src/css/bootstrap-theme.css" rel="stylesheet">
     <!-- Font-Awesome CSS -->
-    <link href="<?php echo get_template_directory_uri(); ?>/src/css/font-awesome.css" rel="stylesheet">
+    <!-- <link href="<?php echo get_template_directory_uri(); ?>/src/css/font-awesome.css" rel="stylesheet"> -->
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
     <!-- Styles core CSS -->
     <link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet">
     <!--Jquery-->
@@ -60,44 +61,43 @@ include($path_dom);
 				<div class="col-lg-3 col-sm-1"></div>
 				<div class="col-lg-7 col-sm-9 thumbnail voffset7" id="vcard">
 					<div class="row">
-						<div class="col-lg-6 col-md-7" style="margin-top: -40px;">
+						<div class="col-lg-6 col-md-7 col-md-offset-3" style="margin-top: -40px;">
 						<a class="" href="" title="Isman Siete">
 							<img class="img-thumbnail img-responsive" src="https://avatars3.githubusercontent.com/u/9478220?v=3&s=460" width="" alt="">
 						</a>
 						</div>
-						<div class="col-lg-1"></div>
-						<div class="col-lg-5 text-left"  style="margin-top: -10px;">
-						<h4><strong>Ismael Benito</strong></h4>
-						<p>Intento de físico e ingeniero electrónico de telecomunicaciones. </p>
-						</div>
 					</div>
-					<div class="row">
-					<div class="col-md-1"></div>
-					<div class="col-md-10">
-					
-						
-							
+					<div class="row voffset3">
+					<div class="col-md-10 col-md-offset-1">
 						<!--<i class="fa fa-twitter"></i>-->
 						 <!-- Nav tabs -->
 						  <ul class="nav nav-tabs" role="tablist">
-							<li role="presentation" class="active"><a href="#twitter" aria-controls="home" role="tab" data-toggle="tab"><span class="fa-lg fa-twitter"></a></li>
-							<li role="presentation"><a href="#github" aria-controls="profile" role="tab" data-toggle="tab"><span class="fa-lg fa-github"></a></li>
-							<li role="presentation"><a href="#instagram" aria-controls="messages" role="tab" data-toggle="tab"><span class="fa-lg fa-instagram"></a></li>
-							<li role="presentation"><a href="#linkedin" aria-controls="messages" role="tab" data-toggle="tab"><span class="fa-lg fa-linkedin"></a></li>
+							<li role="presentation" class="active"><a href="#user" aria-controls="user" role="tab" data-toggle="tab"><span class="fa fa-lg fa-user"></span></a></li>
+							<li role="presentation"><a href="#twitter" aria-controls="twitter" role="tab" data-toggle="tab"><span class="fa fa-lg fa-twitter"></span></a></li>
+							<li role="presentation"><a href="#github" aria-controls="github" role="tab" data-toggle="tab"><span class="fa fa-lg fa-github"></span></a></li>
+							<li role="presentation"><a href="#linkedin" aria-controls="linkedin" role="tab" data-toggle="tab"><span class="fa fa-lg fa-linkedin"></span></a></li>
 						  </ul>
 
 						<!-- Tab panes -->
 						  <div class="tab-content">
-							<div role="tabpanel" class="tab-pane fade in active" id="twitter">
-								</span> @ismansiete</h4>
-								</a>
+							<div role="tabpanel" class="tab-pane fade in active" id="user">
+								<h4><strong>Ismael Benito</strong></h4>
+								<?php 
+									$html_twitter = file_get_html('https://twitter.com/ismansiete'); 
+									foreach($html_twitter->find('p.ProfileHeaderCard-bio') as $e){
+										echo '<p>'. strip_tags($e->innertext) .'</p>';
+									}
+								?>
+								
+							</div>
+							<div role="tabpanel" class="tab-pane fade" id="twitter">
+								<h4><strong><a href="https://twitter.com/ismansiete">@ismansiete</a></strong></h4>
 								<?php
-								$html_twitter = file_get_html('https://twitter.com/ismansiete'); 
 								$i = 0;
 								echo '<div class="row text-center">';
 								foreach($html_twitter->find('span.ProfileNav-value') as $e){
 								echo '<div class="col-lg-4">';
-								echo '<h4>'. $e->innertext . '</h4>';
+								echo '<h4><strong>'. $e->innertext . '</strong></h4>';
 								$i++;
 									switch ($i) {
 									case 1:
@@ -107,7 +107,7 @@ include($path_dom);
 										echo '<span class="text-muted">Siguiendo</span></div>';
 										break 1;
 									case 3:
-										echo '<span class="text-muted">Siguidores</span></div>';
+										echo '<span class="text-muted">Seguidores</span></div>';
 										break 2;
 									default:
 										break;
@@ -117,21 +117,35 @@ include($path_dom);
 								?>
 							</div>
 							<div role="tabpanel" class="tab-pane fade" id="github">
-							</span> isman7</h4>
-								</a>
+								<h4><strong><a href="https://github.com/isman7">@isman7</a></strong></h4>
 								<?php
 								$html_github = file_get_html('https://github.com/isman7');
 								$i = 0;
 								echo '<div class="row text-center">';
 								foreach($html_github->find('strong.vcard-stat-count') as $e)
-								$e->outertext = '<h4>'.$e->innertext.'</h4>';
+								$e->outertext = '<h4><strong>'.$e->innertext.'</strong></h4>';
 								foreach($html_github->find('a.vcard-stat') as $e){
 								echo '<div class="col-lg-4">' . $e->innertext . '</div>';
 								}
 								echo '</div>'; /* row */
 								?>
 							</div>
-							<div role="tabpanel" class="tab-pane fade" id="instagram">ola k ase</div>
+							<div role="tabpanel" class="tab-pane fade" id="linkedin">
+								<h4><strong><a href="https://www.linkedin.com/in/ismael-benito-altamirano-8a41b963">Ismael Benito Altamirano</strong></h4>
+								<?php
+								$html_linkedin = file_get_html('https://www.linkedin.com/in/ismael-benito-altamirano-8a41b963');
+								$i = 0;
+								echo '<div class="row text-center">';
+								//$eles = $html_linkedin->find('*');
+								
+								foreach($html_linkedin->find('div.member-connections') as $e){
+								
+								echo '<h4>'. $e->innertext . '</h4>';
+
+								}
+								echo '</div>'; /* row */
+								?>
+							</div>
 						  </div>
 
 					
